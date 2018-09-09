dc6a9630f811ec8ec37ba743ebbe200e5c0e1a41
nasz_login_do_github <nasz_email_do_github>
2018-09-09 18:44:34 +0200

    refactor

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,28 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+import com.kodilla.testing.calculator.Calculator;
 
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+public class TestingMain {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(7, 7);
+        int subtractionResult = calculator.subtraction(7, 5);
+
+        if ((addResult == 14) && (subtractionResult == 2)) {
+            System.out.println("class Calculator OK");
+        } else {
+            System.out.println("class Calculator error");
+        }
     }
 }
