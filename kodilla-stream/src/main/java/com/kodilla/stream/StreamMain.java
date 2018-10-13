014f717f52423770af4a2dbd10b0d19aa16002d6
nasz_login_do_github <nasz_email_do_github>
2018-10-13 23:20:54 +0200

    Moduł 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,46 @@
+package com.kodilla.stream;
+
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text"));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println("Upiekszanie tekstu");
+        String x = poemBeautifier.beautify("upiekszac", (text -> "ABC" + text));
+        System.out.println(x);
+        String y = poemBeautifier.beautify("upiekszac", (text -> text.toUpperCase()));
+        System.out.println(y);
+        String a = poemBeautifier.beautify("upiekszac", (text -> String.valueOf(text.length())));
+        System.out.println(a);
+        String b = poemBeautifier.beautify("upiekszac", (text -> text.replace("u", "U")));
+        System.out.println(b);
+    }
+}
